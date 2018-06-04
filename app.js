8a34600b110488dce59f69498e383e3a6f2dd818
David Abdelmalek <davidonsy123@gmail.com>
2018-06-04 11:32:27 +0200

    Secnd

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const config = require('./config');
+const apiai = require('apiai');
+const sessionIds = new Map();
+const uuid = require('uuid')
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen((process.env.PORT || 5000));
+
+
+//Connecting between Facebook messenger and API.AI
+const apiAiService = apiai(config.API_AI_CLIENT_ACCESS_TOKEN, {
+    language: "en",
+    requestSource: "fb"
+});
+
 
 //default route
 app.get('/',(req,res)=>{
@@ -17,7 +28,7 @@ app.get('/',(req,res)=>{
 //Validate that Facebook webhook is correct!
 app.get("/webhook", function (req, res) {
     if (req.query['hub.mode'] && req.query["hub.verify_token"] === process.env.Verify_TOKEN) {
-        console.log("Verified webhook");
+      //  console.log("Verified webhook");
         res.status(200).send(req.query["hub.challenge"]);
     } else {
         console.error("Verification failed. The tokens do not match.");
@@ -27,14 +38,17 @@ app.get("/webhook", function (req, res) {
 
 //Handling Messages
 app.post('/webhook',function(req,res){
-    console.log(req.body);
+	console.log(JSON.stringify(req.body));
+   
     //if the trigger comes from Page
     if (req.body.object === 'page') {
+   
         //check if we have multiple entries
         req.body.entry.forEach(function (entry) {
             entry.messaging.forEach(function( event){
                 if (event.message && event.message.text) {
-                    sendMessage(event);
+                    console.log("Berrrr");
+                    receivedMessage(event);
                 }
             });
         });
@@ -42,91 +56,131 @@ app.post('/webhook',function(req,res){
     }
 });
 
-
-function sendMessage(event) {
-    let sender = event.sender.id;
-    let text = event.message.text;
-
-    let apiai = apiaiApp.textRequest(text, {
-        sessionId: 'tabby_cat' // use any arbitrary id
-    });
-
-    apiai.on('response', (response) => {
-        // Got a response from api.ai. Let's POST to Facebook Messenger
-    });
-
-    apiai.on('error', (error) => {
-        console.log(error);
-    });
-
-    apiai.end();
+function sendTextMessage(recipientId, text) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: text
+        }
+    }
+    callSendAPI(messageData);
 }
 
-//setting configuration between API.AI and Facebook Messanger
-var apiai = require('apiai');
-const apiaiApp = apiai(config.API_AI_CLIENT_ACCESS_TOKEN);
 
 
-function sendMessage(event) {
-    let sender = event.sender.id;
-    let text = event.message.text;
+function receivedMessage(event) {
+
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
 
 
-    let request = apiaiApp.textRequest(text, {
-        sessionId: 'David' 
+    console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    //if sender is not exists in map, set the sender as a key to random number as a value;
+    if (!sessionIds.has(senderID)) {
+        sessionIds.set(senderID, uuid.v1());
+    }
+    //Handling Quick Reply
+    //var quickReply = message.quick_reply;
+    // if (quickReply) {
+     //   handleQuickReply(senderID, quickReply, messageId);
+     //   return;
+   // }
+
+    //Handling message Text
+    var messageText = message.text;
+    sendToApiAi(senderID, messageText);
+}
+
+
+
+function sendToApiAi(sender, text) {
+
+    //to make a request to api, we set a text and session it
+    //https://dialogflow.com/docs/reference/api-v2/rest/v2beta1/WebhookRequest
+    let apiaiRequest = apiAiService.textRequest(text, {
+        sessionId: sessionIds.get(sender)
     });
 
-    //get the response from apiAi
-    request.on('response', function (response) {
-        console.log(response);
-        let aiText = response.result.fulfillment.speech;
-
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {
-                access_token: config.PAGE_ACCESS_TOKEN
-            },
-            method: 'POST',
-            json: {
-                recipient: {
-                    id: sender
-                },
-                message: {
-                    text: aiText
-                }
-            }
-        }, (error, response) => {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        });
+    apiaiRequest.on('response', (response) => {
+            handleApiAiResponse(sender, response);
     });
 
+    apiaiRequest.on('error', (error) => console.error(error));
+    apiaiRequest.end();
+}
 
+
+
+function handleApiAiResponse(sender, response) {
+    let responseText = response.result.fulfillment.speech;
+    let responseData = response.result.fulfillment.data;
+    let messages = response.result.fulfillment.messages;
+  //  let action = response.result.action;
+  //  let contexts = response.result.contexts;
+   // let parameters = response.result.parameters;
+
+   if (responseText == '' && !isDefined(action)) {
+        //api ai could not evaluate input.
+        console.log('Unknown query' + response.result.resolvedQuery);
+        sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
+    } else if (isDefined(responseData) && isDefined(responseData.facebook)) {
+        try {
+            console.log('Response as formatted message' + responseData.facebook);
+            sendTextMessage(sender, responseData.facebook);
+        } catch (err) {
+            sendTextMessage(sender, err.message);
+        }
+    } else if (isDefined(responseText)) {
+
+        sendTextMessage(sender, responseText);
+    }
+}
+
+function callSendAPI(messageData) {
     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {
-            access_token: PAGE_ACCESS_TOKEN
+            access_token: config.PAGE_ACCESS_TOKEN
         },
         method: 'POST',
-        json: {
-            recipient: {
-                id: sender
-            },
-            message: {
-                text: text
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            if (messageId) {
+                console.log("Successfully sent message with id %s to recipient %s",
+                    messageId, recipientId);
+            } else {
+                console.log("Successfully called Send API for recipient %s",
+                    recipientId);
             }
-        }
-    }, function (error, response) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
+        } else {
+            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
         }
     });
 }
+
+function isDefined(obj) {
+    if (typeof obj == 'undefined') {
+        return false;
+    }
+
+    if (!obj) {
+        return false;
+    }
+
+    return obj != null;
+}
+
 
 
 console.log("hello");
